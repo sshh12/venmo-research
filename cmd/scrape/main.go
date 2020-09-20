0eb84a61e7187f80ca2e745985a0673b26ce45e6
Shrivu Shankar <shrivu1122@gmail.com>
2020-09-20 12:30:51 -0500

    added code

---
+++ cmd/scrape/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/sshh12/research/venmo"
+)
+
+type workerTask struct {
+	Start  int
+	End    int
+	Path   string
+	Shard  int
+	Shards int
+}
+
+func main() {
+
+	var token string
+	var savePath string
+	flag.StringVar(&token, "token", "", "venmo token")
+	flag.StringVar(&savePath, "save_path", ".", "path to download to")
+	shardIdx := flag.Int("shard_idx", 0, "shard index")
+	shardCnt := flag.Int("shard_cnt", 1, "total shards")
+	startID := flag.Int("start_id", 0, "venmo id to start from")
+	endID := flag.Int("end_id", 90000000, "venmo id to start from")
+	interval := flag.Int("interval_size", 10000, "number of ids per file")
+	flag.Parse()
+	if token == "" {
+		panic("token is required")
+	}
+
+	client := venmo.NewClient(token)
+	workerCnt := 5
+	tasks := make(chan workerTask)
+	complete := make(chan bool)
+	for j := 0; j < workerCnt; j++ {
+		go worker(client, tasks, complete)
+	}
+	for i := *startID; i < *endID; i += *interval {
+		tasks <- workerTask{Start: i, End: i + *interval, Path: savePath, Shard: *shardIdx, Shards: *shardCnt}
+	}
+	close(tasks)
+	for j := 0; j < workerCnt; j++ {
+		<-complete
+	}
+}
+
+func worker(client *venmo.Client, tasks <-chan workerTask, complete chan<- bool) {
+	for task := range tasks {
+		fmt.Printf("Worker Started [%d, %d)\n", task.Start, task.End)
+		downloadFeedRange(client, task.Start, task.End, task.Path, task.Shard, task.Shards)
+		fmt.Printf("Worker Finished [%d, %d)\n", task.Start, task.End)
+	}
+	complete <- true
+}
+
+func downloadFeedRange(client *venmo.Client, start int, end int, savePath string, shard int, shards int) {
+	tempFn := path.Join(savePath, fmt.Sprintf("%d-%d.venmo-raw", start, end))
+	fp, err := os.Create(tempFn)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+	for i := start; i < end; i++ {
+		if i%shards != shard {
+			continue
+		}
+		feed, err := client.FetchFeed(i)
+		if err != nil {
+			fmt.Println("failed @", i)
+			panic(err)
+		}
+		for _, item := range feed {
+			encoded, _ := json.Marshal(item)
+			if _, err := fp.WriteString(string(encoded) + "\n"); err != nil {
+				panic(err)
+			}
+		}
+	}
+}
---
+++ venmo/client.go
@@ -0,0 +1,137 @@
+package venmo
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+const defaultUserAgent string = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+
+// Client is an instance for webscraping venmo
+type Client struct {
+	httpClient *http.Client
+	reqCnt     uint64
+	token      string
+	mux        sync.Mutex
+}
+
+type venmoFeed struct {
+	Data   []venmoFeedItem `json:"data"`
+	Paging venmoPaging     `json:"paging"`
+}
+
+type venmoFeedItem struct {
+	PaymentID    int                `json:"payment_id"`
+	StoryID      string             `json:"story_id"`
+	Message      string             `json:"message"`
+	Type         string             `json:"type"`
+	Actor        venmoUser          `json:"actor"`
+	Transactions []venmoTransaction `json:"transactions"`
+	Created      string             `json:"created_time"`
+	Updated      string             `json:"updated_time"`
+}
+
+type venmoPaging struct {
+	NextURL string `json:"next"`
+	PrevURL string `json:"previous"`
+}
+
+type venmoTransaction struct {
+	Target interface{} `json:"target"`
+}
+
+type venmoUser struct {
+	Username   string `json:"username"`
+	PictureURL string `json:"picture"`
+	Name       string `json:"name"`
+	FirstName  string `json:"firstname"`
+	LastName   string `json:"lastname"`
+	Created    string `json:"date_created"`
+	IsBusiness bool   `json:"is_business"`
+	Canceled   bool   `json:"cancelled"`
+	ID         string `json:"id"`
+	ExternalID string `json:"external_id"`
+}
+
+// NewClient creates a venmo client
+func NewClient(token string) *Client {
+	client := &Client{token: token, httpClient: &http.Client{}}
+	return client
+}
+
+// FetchFeed gets the feed of a user
+func (client *Client) FetchFeed(userID int) ([]venmoFeedItem, error) {
+	var body venmoFeed
+	err := client.doRateLimitedRequest("GET", fmt.Sprintf("https://venmo.com/api/v5/users/%d/feed", userID), &body)
+	if err != nil {
+		return nil, err
+	}
+	items := body.Data
+	curURL := body.Paging.PrevURL
+	for curURL != "" {
+		var pageBody venmoFeed
+		err := client.doRateLimitedRequest("GET", curURL, &pageBody)
+		if err != nil {
+			return nil, err
+		}
+		if len(pageBody.Data) == 0 {
+			break
+		}
+		items = append(items, pageBody.Data...)
+		curURL = pageBody.Paging.PrevURL
+	}
+	curURL = body.Paging.NextURL
+	for curURL != "" {
+		var pageBody venmoFeed
+		err := client.doRateLimitedRequest("GET", curURL, &pageBody)
+		if err != nil {
+			return nil, err
+		}
+		if len(pageBody.Data) == 0 {
+			break
+		}
+		items = append(items, pageBody.Data...)
+		curURL = pageBody.Paging.NextURL
+	}
+	return items, nil
+}
+
+func (client *Client) doRateLimitedRequest(method string, url string, respType interface{}) error {
+	client.mux.Lock()
+	client.mux.Unlock()
+	err := client.doRequest(method, url, respType)
+	if err != nil {
+		client.mux.Lock()
+		for err != nil {
+			fmt.Println("Rate limited, waiting.", err, url)
+			time.Sleep(5 * time.Minute)
+			err = client.doRequest(method, url, respType)
+		}
+		client.mux.Unlock()
+		fmt.Println("Rate limited done.")
+	}
+	return nil
+}
+
+func (client *Client) doRequest(method string, url string, respType interface{}) error {
+	req, err := http.NewRequest(method, url, nil)
+	req.Header.Add("User-Agent", defaultUserAgent)
+	req.AddCookie(&http.Cookie{Name: "api_access_token", Value: client.token})
+	if err != nil {
+		return err
+	}
+	resp, err := client.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	// temp, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(temp))
+	if err := json.NewDecoder(resp.Body).Decode(&respType); err != nil {
+		return err
+	}
+	return nil
+}
